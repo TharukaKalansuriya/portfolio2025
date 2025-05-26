a5b19ffeec19db22c5bdec12def68193fb6a17bf
Tharuka Thathsara Kalansuriya <145830222+TharukaKalansuriya@users.noreply.github.com>
2025-05-26 11:48:37 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -713,8 +713,7 @@ export default function Portfolio() {
                         <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                           <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
                         </svg>
-                      </div>
-                      <a href="tel:+94775651243" className="hover:underline">+94 77 565 1243</a>
+                      </div
                     </div>
                     
                     <div className="flex items-center gap-3">
@@ -724,7 +723,7 @@ export default function Portfolio() {
                           <circle cx="12" cy="10" r="3"></circle>
                         </svg>
                       </div>
-                      <address className="not-italic">Welterfreedon Gardens, Ganegoda, Akmeemana, Galle, Sri Lanka, 80090</address>
+                      <address className="not-italic"> Galle, Sri Lanka</address>
                     </div>
                     
                     <div className="mt-8 flex space-x-4">
